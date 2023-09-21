3d61a0814da51e1e26e417f40889117ac3299a8f
Dj-Polyester <anonymous_571@outlook.com>
2023-09-21 03:30:02 +0300

    rm unnecessary samples

--- plot_sample.py
+++
@@ -1,9 +0,0 @@
-# %%
-from torch_wrapper.analysis import Plotter
-from torch_wrapper.tuning import Metric
-
-# %%
-plotter = Plotter()
-
-# %%
-plotter.plot2d([Metric.Accuracy, Metric.LOSS], filename="results.txt")
